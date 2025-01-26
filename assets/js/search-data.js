1dd12b48a574ed25a9973bb95936ee84a75e9b1c
Braghadeeshln <94704334+Braghadeeshln@users.noreply.github.com>
2025-01-26 20:06:30 +0000

    Deploying to gh-pages from @ Braghadeeshln/Braghadeeshln.github.io@c294f2745a9f3e48dd9a3a45bfb9603301cce06e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -485,14 +485,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%62%6C%61%6B@%6B%74%68.%73%65", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/braghadeesh-lakshminarayanan-002b73ba", "_blank");
         },
       },{
         id: 'social-rss',
@@ -506,14 +506,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=8erG-XQAAAAJ&hl=en", "_blank");
         },
       },{
       id: 'light-theme',
