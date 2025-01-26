afcbfcba11061694d9897137e5738420f90040f2
Braghadeeshln <94704334+Braghadeeshln@users.noreply.github.com>
2025-01-26 20:59:17 +0000

    Deploying to gh-pages from @ Braghadeeshln/Braghadeeshln.github.io@5d1af6dcaa437ee8c84e98d51faa19e80c0bf7a4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
+        },{id: "nav-repositories",
+          title: "repositories",
+          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/repositories/";
+          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "Materials for courses you taught. Replace this text with your description.",
